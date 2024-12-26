a8d3daccf90fe05914e3be15680b9b679b3ec2a6
Allan Lewis <allan.e.lewis@gmail.com>
2024-12-26 12:36:39 -0500

    graphql

---
+++ components/batch-card.tsx
@@ -0,0 +1,7 @@
+import { Batch } from "./brewfather-batches"
+
+export default function BatchCard({batch} : {batch: Batch}) {
+    return (
+        <div>{ batch.batchName }</div>
+    )
+  }
--- components/brewfather-batches.tsx
+++ components/brewfather-batches.tsx
@@ -1,12 +1,41 @@
+import { getClient, query } from '../lib/apollo-client';
+import { gql } from "@apollo/client";
+import BatchCard from "./batch-card"
 
-export default function BrewfatherBatches() {
+const USER_LIST_QUERY = gql`
+  query UserList {
+    batches(batchDate: 0) {
+      batchId
+      batchName
+    }
+  }
+`;
+
+export interface Batch {
+  batchId: string,
+  batchName: string
+}
+
+export interface GetBatchesResponse {
+  batches: Batch []
+}
+
+export default async function BrewfatherBatches() {
+  const { data, loading, error } = await query<GetBatchesResponse>({ query: USER_LIST_QUERY });
+
+  if (loading) return <h1>Loading...</h1>
+  if (error) return <h1>Error: {error.message}</h1>
+
   return (
     <div className="rounded-t-md bg-neutral-200 p-4">
         <h3 className="text-xl">Brewfather Batches</h3>
         <div>
-            <p className="text-center py-16">
+      {data.batches.map(batch => (
+        <BatchCard key={batch.batchId} batch={batch}></BatchCard> 
+      ))}
+            {/* <p className="text-center py-16">
                 <em>No Brewfather batch data found.</em>
-            </p>
+            </p> */}
         </div>
     </div>
   )
